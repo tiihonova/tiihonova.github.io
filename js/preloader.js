41d5a4145760790a9fb5d642fcb220c1ded8f700
tiihonova <49042796+tiihonova@users.noreply.github.com>
2021-10-03 22:05:02 +0400

    ergo

--- js/preloader.js
+++
@@ -1,9 +0,0 @@
-jQuery(function () {
-  $("body").addClass("scroll_disable");
-
-  $(".preloader").delay(2000).fadeOut(1000);
-
-  setTimeout(function () {
-    $("body").removeClass("scroll_disable");
-  }, 2000);
-});
